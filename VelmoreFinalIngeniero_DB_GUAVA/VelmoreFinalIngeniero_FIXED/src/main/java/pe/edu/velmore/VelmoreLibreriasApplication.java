b347da3ef8377599d9a632e2f460e72c1a3a6fc0
Cesar Edu Palomino Berrocal <edupalomino728@gmail.com>
2026-06-18 12:23:24 -0500

    Add files via upload

---
+++ VelmoreFinalIngeniero_DB_GUAVA/VelmoreFinalIngeniero_FIXED/src/main/java/pe/edu/velmore/VelmoreLibreriasApplication.java
@@ -0,0 +1,11 @@
+package pe.edu.velmore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelmoreLibreriasApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VelmoreLibreriasApplication.class, args);
+    }
+}
